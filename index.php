01badd76159bbb219fd425fcbe6c49691ecab2e5
Joseph Winterhalter <jwint162@gmail.com>
2018-08-08 15:32:31 -0700

    added responsiveness

--- index.php
+++ index.php
@@ -6,160 +6,190 @@
 /_/ /_/\___/_/_/\____/ 
 -->                        
 
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Joseph Winterhalter</title>
+	<meta name="viewport" content="width=device-width, initial-scale=2.0">
 
-<style>
-	a{
-		font-size:12px;
-		text-decoration:none;
-	}
+</head>
+<body>
 
-	h1{
 
-		font-size:18px;
-	}
+	<style>
+		a{
+			font-size:14px;
+			text-decoration:none;
+		}
 
-	h2{
-		font-size:16px;
-		padding:0px;
-		margin:0px;
-	}
+		h1{
 
-	td{
-		vertical-align: top;
-	}
+			font-size:20px;
+		}
 
-	html{
+		h2{
+			font-size:16px;
+			padding:0px;
+			margin:0px;
+		}
 
-	}
+		td{
+			vertical-align: top;
+		}
 
-	body{
-		font-family:arial;
-	}
+		html{
 
-	.hi{
-		margin-top:-5px; 
-		color:#999; 
-		margin-left:15px; 
-		font-size:12px;
-	}
+		}
 
-	.hold{
-		width:610px; 
-		height:600px; 
-		margin-left:auto; 
-		margin-right:auto; 
-		padding:0px; 
-		border-radius:3px; 
-		padding:20px; 
-		margin-top:30px;
-	}
+		body{
+			font-family:arial;
+			margin:0px;
+			padding:0px;
+		}
 
-</style>
+		.hi{
+			margin-top:-5px; 
+			color:#999; 
+			margin-left:15px; 
+			font-size:14px;
+		}
 
-<div class="hold" >
+		.hold{
+border-radius:10px; padding:10px; width:100%; max-width:700px; margin-left:auto; margin-right:auto; border:1px #ccc dashed; font-size:14px; margin-top:20px;
+		}
 
-	<table style="border-bottom:1px #ccc solid; padding-bottom:20px;">
-		<tr>
-			<td>
-				<div style=" padding-top:0px;">
-					<img src="/jjsmall.png"  style="width:105px; border-radius:100px;"/> 
-				</div>
-			</td>
-			<td>
-				<div style=" margin-top:0px;">
+		.cell{
+			width:48%;
+			float:left;
+			margin-top:10px;
+			margin-bottom:10px;
+		}
 
-					<div style=" padding-top:0px; margin-top:-10px;">
-						<h1 style="margin-left:15px;">Joseph Winterhalter</h1>
-					</div>
-					<div>
-					</div>
-					<div class="hi">
-						Dropped out of Clemson University in 2004 after seeing thefacebook.com. Launched Peerprofile.com to compete with Facebook but unable to catch their traction. Created Testrific a social entertainment portal which grew to have 25 Million+ registered users. Have since been involved in a decent number of startups & lately obsessed with blockchain technology.
-					</div>
-				</div>
+		.cell1{
+		}
+
+		.cell2{
+
+		}
+
+		.clearfix:after {
+		  content: "";
+		  display: table;
+		  clear: both;
+		}
+
+		  
+		@media only screen and (min-width : 768px){
+				width:50%;
+		}
+		  
+		@media only screen and (max-width : 767px) {
+			.cell{
+				width:100%;
+			}
+		}
+
+
+	</style>
+
+
+
+
+<div class="clearfix hold" style="">
+
+	<div class="clearfix" style="width:100%; margin-top:-10px;">
+		<div>
+			<h1 style="margin-left:5px;">Joseph Winterhalter</h1>
+		</div>
+
+		<table>
+			<tr>
+				<td style="padding-left:0px;">
+					<img src="/jjsmall.png"  style="width:85px; border-radius:100px;"/> 
+				</td>
+				<td style="padding-left:10px; padding-right:20px; color:#777;">
+
+					Dropped out of Clemson University in 2004 after seeing thefacebook.com. Launched Peerprofile.com to compete with Facebook but unable to catch their traction. Created Testrific a social entertainment portal which grew to have 25 Million+ registered users. Have since been involved in a decent number of startups & lately obsessed with blockchain technology.
+
 			</td>
 		</tr>
 	</table>
 
+	</div>
 
-	<table style="width:100%; font-size:12px;">
-		<tr>
-			<td style="width:50%; ">
-				<div style="margin-top:15px;">
-					<h2>Writings</h2>
-				</div>
-				<div style="margin-top:10px;">
-					Visions on decentralized consensus <br />
-					<span style="color:#aaa;">( Coming soon )</span>
-				</div>
-			</td>
-			<td style="width:50%; ">
+	<div class="cell">
+		<div style="margin-top:15px;">
+			<h2>Writings</h2>
+		</div>
+		<div style="margin-top:10px;">
+			Visions on decentralized consensus <br />
+			<span style="color:#aaa;">( Coming soon )</span>
+		</div>
+	</div>
 
-
-				<div style="padding-bottom:30px; margin-top:-10px; padding-top:5px;">
-					<div style="margin-top:20px; ">
-						<h2>Projects</h2>
-					</div>
-
-					<div >
-
-					<div style="margin-top:10px;">
-						<a style="text-decoration:none;" href="http://www.coinworth.io">CoinWorth.io</a> - Coin Price Prediction Market
-					</div>
-
-					<div style="margin-top:10px;">
-						<a style="text-decoration:none;" href="http://www.galaxus.io">Galaxus.io</a> - Browser Based MMORPG
-					</div>
-
-					<div style="margin-top:10px;">
-						<a style=" text-decoration:none;" href="http://www.galaxus.io">Votable.com</a> - Community Pages
-					</div>
-
-					<div style="margin-top:10px;">
-						Project Life - Semantic life logic interpreter
-					</div>
-
-					<div style="margin-top:10px;">
-						<a style="text-decoration:none;" href="http://www.galaxus.io">Scholarspot.com</a> - Free University
-					</div>
-
-					<div style="margin-top:10px;">
-						<a style="text-decoration:none;" href="http://www.galaxus.io">Testriffic.com</a> - Social Entertainment Portal.
-					</div>
-
-				</div>
-
-			</td>
-
-		</tr>
-	</table>
-
-
-
-	<table style="width:100%;">
-		<tr>
-			<td style="width:50%; ">
-
-				<div>
-					<h2 style="margin-top:3px;">Music</h2>
-					<div style="margin-top:7px;"><a href="/dj">Here</a></div>	
-				</div>
-
-			</td>
-			<td style="width:50%; ">
-
-			<div style="margin-top:3px; ">
-				<h2>Contact</h2>
-			</div>
-			<div style="margin-top:5px;">
-				<a href="http://www.twitter.com/archimedes144">Twitter</a> 
-				<a style="margin-left:10px;" href="https://www.linkedin.com/in/joseph-winterhalter">LinkedIn</a> 
+	<div class="cell cell1">
+		<div style="padding-bottom:30px; margin-top:-10px; padding-top:5px;">
+			<div style="margin-top:20px; ">
+				<h2>Projects</h2>
 			</div>
 
+			<div style="margin-top:10px;">
+				<a style="text-decoration:none;" href="http://www.coinworth.io">CoinWorth.io</a> - Coin Price Prediction Market
+			</div>
 
-			</td>
+			<div style="margin-top:10px;">
+				<a style="text-decoration:none;" href="http://www.galaxus.io">Galaxus.io</a> - Browser Based MMORPG
+			</div>
 
-		</tr>
-	</table>
+			<div style="margin-top:10px;">
+				<a style=" text-decoration:none;" href="http://www.galaxus.io">Votable.com</a> - Community Pages
+			</div>
+
+			<div style="margin-top:10px;">
+				Project Life - Semantic life logic interpreter
+			</div>
+
+			<div style="margin-top:10px;">
+				<a style="text-decoration:none;" href="http://www.galaxus.io">Scholarspot.com</a> - Free University
+			</div>
+
+			<div style="margin-top:10px;">
+				<a style="text-decoration:none;" href="http://www.galaxus.io">Testriffic.com</a> - Social Entertainment Portal.
+			</div>
+
+		</div>
+	</div>
+
+	<div class="cell">
+
+		<div>
+			<h2 style="margin-top:3px;">Music</h2>
+			<div style="margin-top:7px;">16 / <a href="/dj">Awakening</a></div>	
+			<div style="margin-top:7px;">15 / <a href="/dj">Colours</a></div>	
+			<div style="margin-top:7px;">14 / <a href="/dj">Rejuvenation</a></div>	
+			<div style="margin-top:10px;">
+				<a style="font-size:12px;" href="/dj">More</a>
+			</div>
+
+
+
+		</div>
+
+	</div>
+
+	<div class="cell">
+
+		<div style="margin-top:3px; ">
+			<h2>Contact</h2>
+		</div>
+		<div style="margin-top:5px;">
+			<a href="http://www.twitter.com/archimedes144">Twitter</a> 
+			<a style="margin-left:10px;" href="https://www.linkedin.com/in/joseph-winterhalter">LinkedIn</a> 
+		</div>
+
+	</div>
 
 </div>
+
+</body>
+</html>
