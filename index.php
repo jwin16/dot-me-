f3df7761f058c86c716730175b212b2e74902abf
Joseph Winterhalter <jwint162@gmail.com>
2018-08-08 14:29:18 -0700

    initial commit

---
+++ index.php
@@ -0,0 +1,165 @@
+<!-- 
+    __  __     ____    
+   / / / /__  / / /___ 
+  / /_/ / _ \/ / / __ \
+ / __  /  __/ / / /_/ /
+/_/ /_/\___/_/_/\____/ 
+-->                        
+
+
+<style>
+	a{
+		font-size:12px;
+		text-decoration:none;
+	}
+
+	h1{
+
+		font-size:18px;
+	}
+
+	h2{
+		font-size:16px;
+		padding:0px;
+		margin:0px;
+	}
+
+	td{
+		vertical-align: top;
+	}
+
+	html{
+
+	}
+
+	body{
+		font-family:arial;
+	}
+
+	.hi{
+		margin-top:-5px; 
+		color:#999; 
+		margin-left:15px; 
+		font-size:12px;
+	}
+
+	.hold{
+		width:610px; 
+		height:600px; 
+		margin-left:auto; 
+		margin-right:auto; 
+		padding:0px; 
+		border-radius:3px; 
+		padding:20px; 
+		margin-top:30px;
+	}
+
+</style>
+
+<div class="hold" >
+
+	<table style="border-bottom:1px #ccc solid; padding-bottom:20px;">
+		<tr>
+			<td>
+				<div style=" padding-top:0px;">
+					<img src="/jjsmall.png"  style="width:105px; border-radius:100px;"/> 
+				</div>
+			</td>
+			<td>
+				<div style=" margin-top:0px;">
+
+					<div style=" padding-top:0px; margin-top:-10px;">
+						<h1 style="margin-left:15px;">Joseph Winterhalter</h1>
+					</div>
+					<div>
+					</div>
+					<div class="hi">
+						Dropped out of Clemson University in 2004 after seeing thefacebook.com. Launched Peerprofile.com to compete with Facebook but unable to catch their traction. Created Testrific a social entertainment portal which grew to have 25 Million+ registered users. Have since been involved in a decent number of startups & lately obsessed with blockchain technology.
+					</div>
+				</div>
+			</td>
+		</tr>
+	</table>
+
+
+	<table style="width:100%; font-size:12px;">
+		<tr>
+			<td style="width:50%; ">
+				<div style="margin-top:15px;">
+					<h2>Writings</h2>
+				</div>
+				<div style="margin-top:10px;">
+					Visions on decentralized consensus <br />
+					<span style="color:#aaa;">( Coming soon )</span>
+				</div>
+			</td>
+			<td style="width:50%; ">
+
+
+				<div style="padding-bottom:30px; margin-top:-10px; padding-top:5px;">
+					<div style="margin-top:20px; ">
+						<h2>Projects</h2>
+					</div>
+
+					<div >
+
+					<div style="margin-top:10px;">
+						<a style="text-decoration:none;" href="http://www.coinworth.io">CoinWorth.io</a> - Coin Price Prediction Market
+					</div>
+
+					<div style="margin-top:10px;">
+						<a style="text-decoration:none;" href="http://www.galaxus.io">Galaxus.io</a> - Browser Based MMORPG
+					</div>
+
+					<div style="margin-top:10px;">
+						<a style=" text-decoration:none;" href="http://www.galaxus.io">Votable.com</a> - Community Pages
+					</div>
+
+					<div style="margin-top:10px;">
+						Project Life - Semantic life logic interpreter
+					</div>
+
+					<div style="margin-top:10px;">
+						<a style="text-decoration:none;" href="http://www.galaxus.io">Scholarspot.com</a> - Free University
+					</div>
+
+					<div style="margin-top:10px;">
+						<a style="text-decoration:none;" href="http://www.galaxus.io">Testriffic.com</a> - Social Entertainment Portal.
+					</div>
+
+				</div>
+
+			</td>
+
+		</tr>
+	</table>
+
+
+
+	<table style="width:100%;">
+		<tr>
+			<td style="width:50%; ">
+
+				<div>
+					<h2 style="margin-top:3px;">Music</h2>
+					<div style="margin-top:7px;"><a href="/dj">Here</a></div>	
+				</div>
+
+			</td>
+			<td style="width:50%; ">
+
+			<div style="margin-top:3px; ">
+				<h2>Contact</h2>
+			</div>
+			<div style="margin-top:5px;">
+				<a href="http://www.twitter.com/archimedes144">Twitter</a> 
+				<a style="margin-left:10px;" href="https://www.linkedin.com/in/joseph-winterhalter">LinkedIn</a> 
+			</div>
+
+
+			</td>
+
+		</tr>
+	</table>
+
+</div>
